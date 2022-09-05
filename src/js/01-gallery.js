a917e93a28034f891434327ee32c71a0ab7587f8
Kyrylo <savchenko.kyrylo@gmail.com>
2022-09-05 16:38:00 +0300

    hw-7

---
+++ src/js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+
+// ---------------------- select html elements ----------------------
+const galleryContainer = document.body.querySelector('.gallery');
+// ---------------------- render image gallery ----------------------
+const renderImageGallery = (galary) => {
+  // make array of divs for gallery elements
+  const items = galary.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+  });
+
+  //render the gallery to perant element
+  galleryContainer.innerHTML = items.join('');
+};
+
+// ---------------------- Add event handlers ----------------------
+
+// add event handler to popup modal full image
+galleryContainer.onclick = (e) => {
+  e.preventDefault();
+
+  if (!e.target.classList.contains('gallery__image')) return;
+
+  // get full image link
+  const currentFullImagePath = e.target.dataset.source;
+
+  const modalImageBox = basicLightbox.create(
+    `
+      <img width="800" height="600" src="${currentFullImagePath}">
+    `,
+    {
+      onShow: () => window.addEventListener('keyup', escKey),
+      onClose: () => window.removeEventListener('keyup', escKey),
+    }
+  );
+
+  const escKey = (e) => {
+    if (e.key === 'Escape') modalImageBox.close();
+  };
+
+  modalImageBox.show();
+};
+
+// ---------------------- Initialize the functionality -----------------
+renderImageGallery(galleryItems);
---
+++ src/js/02-lightbox.js
@@ -0,0 +1,40 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+// ---------------------- select html elements ----------------------
+const galleryContainer = document.body.querySelector('.gallery');
+// ---------------------- render image gallery ----------------------
+const renderImageGallery = (galary) => {
+  // make array of divs for gallery elements
+  const items = galary.map(({ preview, original, description }) => {
+    return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+  });
+
+  //render the gallery to perant element
+  galleryContainer.innerHTML = items.join('');
+
+  //disable the gallery container reload on click
+  galleryContainer.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
+};
+
+// creating SimpleLightbox modal
+const createModal = () => {
+  let gallery = new SimpleLightbox('.gallery a', {
+    // change default caption data atributes to 'alt'
+    captionsData: 'alt',
+    // change default caption delay in milliseconds atributes to 250
+    captionDelay: 250,
+  });
+};
+
+// ---------------------- Initialize the functionality -----------------
+// first render the gallery
+renderImageGallery(galleryItems);
+// then create modal for the gallery
+createModal();
