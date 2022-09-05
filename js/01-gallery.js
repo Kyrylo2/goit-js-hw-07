5db4908511e80964f7bea6a97f9710457a0a48c8
Kyrylo <savchenko.kyrylo@gmail.com>
2022-09-05 20:44:58 +0300

    added native support check lazyload

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,14 +5,15 @@ import { galleryItems } from './gallery-items.js';
 // ---------------------- select html elements ----------------------
 const galleryContainer = document.body.querySelector('.gallery');
 // ---------------------- render image gallery ----------------------
-const renderImageGallery = (galary) => {
+const renderImageGallery = (galary, browserChecker) => {
   // make array of divs for gallery elements
   const items = galary.map(({ preview, original, description }) => {
     return `<div class="gallery__item">
       <a class="gallery__link" href="${original}">
       <img
-        class="gallery__image lazyload"
-        data-src="${preview}"
+        class="gallery__image ${
+          browserChecker === 'true' ? ` lazyload" data-` : `" loading="lazy" `
+        }src="${preview}"
         data-source="${original}"
         alt="${description}"
       />
@@ -22,6 +23,24 @@ const renderImageGallery = (galary) => {
 
   //render the gallery to perant element
   galleryContainer.innerHTML = items.join('');
+};
+
+//checkBrowser and return true if browser is supported and false plus create lazysize script in body otherwise
+const checkBrowser = () => {
+  if ('loading' in HTMLImageElement.prototype) {
+    return true;
+  } else {
+    const script = document.createElement('script');
+    script.src =
+      'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+    script.integrity =
+      'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==';
+    script.crossorigin = 'anonymous';
+    script.referrerpolicy = 'no-referrer';
+
+    document.body.appendChild(script);
+    return false;
+  }
 };
 
 // ---------------------- Add event handlers ----------------------
@@ -53,4 +72,4 @@ galleryContainer.onclick = (e) => {
 };
 
 // ---------------------- Initialize the functionality -----------------
-renderImageGallery(galleryItems);
+renderImageGallery(galleryItems, checkBrowser());
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,12 +5,14 @@ console.log(galleryItems);
 // ---------------------- select html elements ----------------------
 const galleryContainer = document.body.querySelector('.gallery');
 // ---------------------- render image gallery ----------------------
-const renderImageGallery = (galary) => {
+const renderImageGallery = (galary, browserChecker) => {
   // make array of divs for gallery elements
   const items = galary.map(({ preview, original, description }) => {
     return `
       <a class="gallery__item" href="${original}">
-        <img class="gallery__image lazyload" data-src="${preview}" alt="${description}" />
+        <img class="gallery__image ${
+          browserChecker === 'true' ? ` lazyload" data-` : `" loading="lazy" `
+        }src="${preview}" alt="${description}" />
       </a>`;
   });
 
@@ -33,8 +35,26 @@ const createModal = () => {
   });
 };
 
+//checkBrowser and return true if browser is supported and false plus create lazysize script in body otherwise
+const checkBrowser = () => {
+  if ('loading' in HTMLImageElement.prototype) {
+    return true;
+  } else {
+    const script = document.createElement('script');
+    script.src =
+      'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+    script.integrity =
+      'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==';
+    script.crossorigin = 'anonymous';
+    script.referrerpolicy = 'no-referrer';
+
+    document.body.appendChild(script);
+    return false;
+  }
+};
+
 // ---------------------- Initialize the functionality -----------------
 // first render the gallery
-renderImageGallery(galleryItems);
+renderImageGallery(galleryItems, checkBrowser());
 // then create modal for the gallery
 createModal();
